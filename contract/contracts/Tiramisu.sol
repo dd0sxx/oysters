f1f43c5566eddda7a92693be51c147306efdb33e
gwro <grzeg.wrobel@gmail.com>
2022-03-29 10:27:54 +0200

    remove hashing and change vars to public

--- contract/contracts/Tiramisu.sol
+++ contract/contracts/Tiramisu.sol
@@ -8,21 +8,21 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 contract Tiramisu is ERC721, Ownable {
     using Strings for uint256;
 
-    string private _baseTokenURI;
-    uint16 private _tokenSupply;
-    bool private _premintPhase = true;
-    bytes32 private _root; // merkle root set in constructor
-    mapping(bytes32 => bool) private _claimedWL; // stores addresses that have claimed whitelisted tokens
+    string public baseTokenURI;
+    uint16 public tokenSupply;
+    bool public premintPhase = true;
+    bytes32 public root; // merkle root set in constructor
+    mapping(address => bool) public claimedWL; // stores addresses that have claimed whitelisted tokens
 
     uint16 public constant MAX_SUPPLY = 1000;
     uint64 public constant PRICE = 0.1 ether;
-    address private constant ADDR_80 = 0x95645e9fCfEe7882DA368963d5A460308df24DD6;
-    address private constant ADDR_20 = 0x705a47eBC6fCE487a3C64A2dA64cE2E3B8b2EF55;
+    address public constant ADDR_80 = 0x95645e9fCfEe7882DA368963d5A460308df24DD6;
+    address public constant ADDR_20 = 0x705a47eBC6fCE487a3C64A2dA64cE2E3B8b2EF55;
 
     constructor(string memory baseURI, bytes32 initialRoot) ERC721('Tiramisu Recipe by STILLZ', 'TMISU') {
-        _baseTokenURI = baseURI;
+        baseTokenURI = baseURI;
 
-        uint16 mintIndex = _tokenSupply;
+        uint16 mintIndex = tokenSupply;
         for (uint16 i; i < 10; i++) {
             _incrementTokenSupply();
             _safeMint(ADDR_80, mintIndex + i);
@@ -33,32 +33,28 @@ contract Tiramisu is ERC721, Ownable {
 
     /// @dev minting tokens after the premint phase
     function mint() external payable {
-        require(!_premintPhase, 'premint phase');
+        require(!premintPhase, 'premint phase');
         require(msg.value == PRICE, 'incorrect ether amount supplied');
         issueToken(msg.sender);
     }
 
     /// @dev issues token to recipient
     function issueToken(address recipient) private {
-        uint16 mintIndex = _tokenSupply;
+        uint16 mintIndex = tokenSupply;
         require(mintIndex < MAX_SUPPLY, 'exceeds token supply');
         _incrementTokenSupply();
         _safeMint(recipient, mintIndex);
     }
 
     function setBaseURI(string memory baseURI) public onlyOwner {
-        _baseTokenURI = baseURI;
+        baseTokenURI = baseURI;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         string memory json = ".json";
-        string memory baseURI = _baseTokenURI;
+        string memory baseURI = baseTokenURI;
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), json)) : "";
-    }
-
-    function isPremintPhase() public view returns (bool) {
-        return _premintPhase;
     }
 
     function isApprovedForAll(address owner, address operator) public view override returns (bool) {
@@ -77,22 +73,22 @@ contract Tiramisu is ERC721, Ownable {
     }
 
     function setIsPremintPhase(bool _isPremintPhase) public onlyOwner {
-        _premintPhase = _isPremintPhase;
+        premintPhase = _isPremintPhase;
     }
 
     function getTokensLeft() public view returns (uint16) {
-        return MAX_SUPPLY - _tokenSupply;
+        return MAX_SUPPLY - tokenSupply;
     }
 
     function _incrementTokenSupply() private {
         unchecked {
-            _tokenSupply += 1;
+            tokenSupply += 1;
         }
     }
 
 
     function setMerkleRoot(bytes32 newRoot) public onlyOwner {
-        _root = newRoot;
+        root = newRoot;
     }
 
     function _leaf(address account) private pure returns (bytes32) {
@@ -100,16 +96,15 @@ contract Tiramisu is ERC721, Ownable {
     }
 
     function _verify(bytes32 leaf, bytes32[] memory proof) private view returns (bool) {
-        return MerkleProof.verify(proof, _root, leaf);
+        return MerkleProof.verify(proof, root, leaf);
     }
 
     /// @dev allows whitelisted users to claim their tokens during the premint phase
     function redeem(bytes32[] calldata proof) external {
-        require(_premintPhase, 'not a premint phase');
+        require(premintPhase, 'not a premint phase');
         require(_verify(_leaf(msg.sender), proof), "invalid merkle proof");
-        bytes32 hash = keccak256(abi.encodePacked(msg.sender));
-        require(!_claimedWL[hash], "already claimed");
-        _claimedWL[hash] = true;
+        require(!claimedWL[msg.sender], "already claimed");
+        claimedWL[msg.sender] = true;
         issueToken(msg.sender);
     }
 }
